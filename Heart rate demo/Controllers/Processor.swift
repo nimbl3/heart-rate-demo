3e6c1d8ae9b36a0897693c12b19092d6f151864f
Pirush Prechathavanich <pirush@nimbl3.com>
2018-09-20 14:27:27 +0700

    [#160548166] implement processor with math calculation

--- Heart rate demo/Controllers/Processor.swift
+++ Heart rate demo/Controllers/Processor.swift
@@ -6,4 +6,46 @@
 //  Copyright © 2018 nimbl3. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class Processor {
+    
+    private let windowSize = 9
+    private let defaultBPM: UInt8 = 72 // todo: - ?
+    private let calibrateDuration = 90
+    
+    private let filterOrder = 5
+    private let filterLowerBand = 0.04 // 36
+    private let filterUpperBand = 0.2 // 180
+    
+    private var currentCount = 0
+    
+    private var values: [Float] = []
+    private var peaks: [Bool] = []
+    
+    private var bpmValues: [UInt8] = []
+    private var bpmAverageValues: [UInt8] = []
+    
+    func addInput(_ value: Float) {
+        currentCount += 1
+        
+        values += value
+        peaks += false
+        
+        bpmValues += defaultBPM
+        bpmAverageValues += defaultBPM
+        
+        guard currentCount == windowSize else { return }
+        
+        let size = windowSize + 1
+        let x = values.suffix(size)
+        let mean = x.reduce(0, +) / Float(size)
+        
+        let newX = x.map { $0 - mean }
+        
+        
+    }
+    
+    // MARK: - private helper
+    
+}
